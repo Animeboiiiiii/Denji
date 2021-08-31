0517f89da005ed358a96ea497f94af0195866be3
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2021-08-31 10:16:07 +0530

    Update approve.py

--- SaitamaRobot/modules/approve.py
+++ SaitamaRobot/modules/approve.py
@@ -1,11 +1,11 @@
 import html
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot import dispatcher, DRAGONS
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
+from Denji_Robot.modules.disable import DisableAbleCommandHandler
+from Denji_Robot import dispatcher, DRAGONS
+from Denji_Robot.modules.helper_funcs.extraction import extract_user
 from telegram.ext import CallbackContext, run_async, CallbackQueryHandler
-import SaitamaRobot.modules.sql.approve_sql as sql
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.log_channel import loggable
+import Denji_Robot.modules.sql.approve_sql as sql
+from Denji_Robot.modules.helper_funcs.chat_status import user_admin
+from Denji_Robot.modules.log_channel import loggable
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Update
 from telegram.utils.helpers import mention_html
 from telegram.error import BadRequest
