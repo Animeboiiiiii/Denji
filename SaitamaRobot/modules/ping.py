b111acf4e5d542681ca590eaeaf8d20181998afc
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2021-08-31 12:00:38 +0530

    Update ping.py

--- SaitamaRobot/modules/ping.py
+++ SaitamaRobot/modules/ping.py
@@ -5,9 +5,9 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from SaitamaRobot import StartTime, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import sudo_plus
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Denji_Robot import StartTime, dispatcher
+from Denji_Robot.modules.helper_funcs.chat_status import sudo_plus
+from Denji_Robot.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
