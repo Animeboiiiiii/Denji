9a293ff1acd87fc434d7da50b28f3abb249f06e7
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2021-08-31 12:05:11 +0530

    Update rules.py

--- SaitamaRobot/modules/rules.py
+++ SaitamaRobot/modules/rules.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-import SaitamaRobot.modules.sql.rules_sql as sql
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.string_handling import markdown_parser
+import Denji_Robot.modules.sql.rules_sql as sql
+from Denji_Robot import dispatcher
+from Denji_Robot.modules.helper_funcs.chat_status import user_admin
+from Denji_Robot.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
