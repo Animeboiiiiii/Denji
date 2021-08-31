01b9e29fbe128b1404ddde025768353e9a780fb6
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2021-08-31 11:04:58 +0530

    Update cleaner.py

--- SaitamaRobot/modules/cleaner.py
+++ SaitamaRobot/modules/cleaner.py
@@ -1,14 +1,14 @@
 import html
 
-from SaitamaRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from Denji_Robot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from Denji_Robot.modules.disable import DisableAbleCommandHandler
+from Denji_Robot.modules.helper_funcs.chat_status import (
     bot_can_delete,
     connection_status,
     dev_plus,
     user_admin,
 )
-from SaitamaRobot.modules.sql import cleaner_sql as sql
+from Denji_Robot.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
