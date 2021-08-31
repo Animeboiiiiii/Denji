e98e62df26967a91f1fd85991ca07b8528af7c2c
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2021-08-31 12:01:30 +0530

    Update purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from SaitamaRobot import telethn
-from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
+from Denji_Robot import telethn
+from Denji_Robot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
