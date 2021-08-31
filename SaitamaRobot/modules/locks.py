43bce439847469866b5d69b33ae1b7541150d911
Animeboiiiiii <86106325+Animeboiiiiii@users.noreply.github.com>
2021-08-31 11:45:50 +0530

    Update locks.py

--- SaitamaRobot/modules/locks.py
+++ SaitamaRobot/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import SaitamaRobot.modules.sql.locks_sql as sql
-from SaitamaRobot import dispatcher, DRAGONS, LOGGER
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+import Denji_Robot.modules.sql.locks_sql as sql
+from Denji_Robot import dispatcher, DRAGONS, LOGGER
+from Denji_Robot.modules.disable import DisableAbleCommandHandler
+from Denji_Robot.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.connection import connected
-from SaitamaRobot.modules.sql.approve_sql import is_approved
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from Denji_Robot.modules.log_channel import loggable
+from Denji_Robot.modules.connection import connected
+from Denji_Robot.modules.sql.approve_sql import is_approved
+from Denji_Robot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
